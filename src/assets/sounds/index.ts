b09a1caa3ae3e237d15309ef76780dde71befe31
MateusBreno <brenoconstantino80@gmail.com>
2022-06-01 16:29:17 -0300

    pasta sounds

---
+++ src/assets/sounds/index.ts
@@ -0,0 +1,3 @@
+// ./src/assets/sounds/index.ts
+export { default as AlarmDefiniteCalledOpen } from './alarm-definite-called-open.mp3';
+export { default as AlarmTruckHornCalledOpen } from './alarm-truck-horn-called-open.mp3';
